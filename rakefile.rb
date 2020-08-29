35cf6e2b034b2c9c0e6bfbb4c519d90f7900ea45
countless-integers <oxygen-nitrogen@outlook.com>
2020-08-29 17:28:19 +0200

    refactor: minor clean-up of the rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,31 +1,35 @@
-desc 'create a new post'
-task :newPost do
-  require 'date'
-  title = ENV['TITLE']
-  if !title
-      abort("No title given. Pass a title for the post as 'TITLE' env param")
+require 'date'
+
+namespace :post do
+
+  desc 'create a new post'
+  task :create do
+    title = ENV['title']
+    if !title
+        abort("No title given. Pass a title for the post as 'title' env param")
+    end
+    date = "#{Date.today}"
+    slug = "#{date}-#{title.downcase.gsub(/[^\w]+/, '-')}"
+
+    new_post_path = File.join(
+      File.dirname(__FILE__),
+      '_posts',
+      slug + '.md'
+    )
+
+    File.open(new_post_path, "w") do |file|
+      file << <<~TEXT
+      ---
+      layout: post
+      title: #{title}
+      date: #{date}
+      published: false
+      categories:
+      ---
+
+      TEXT
+    end
+
+    system ("#{ENV['EDITOR']} #{new_post_path}")
   end
-  slug = "#{Date.today}-#{title.downcase.gsub(/[^\w]+/, '-')}"
-  date = "#{Date.today}"
-
-  file = File.join(
-    File.dirname(__FILE__),
-    '_posts',
-    slug + '.markdown'
-  )
-
-  File.open(file, "w") do |f|
-    f << <<-EOS.gsub(/^    /, '')
-    ---
-    layout: post
-    title: #{title}
-    date: #{date}
-    published: false
-    categories:
-    ---
-
-    EOS
-  end
-
-  system ("#{ENV['EDITOR']} #{file}")
 end
